c380d09efc4b41c88408971cfe95ab065a17f1cd
SinhaVinit <vinit.8444935532@gmail.com>
2022-02-19 20:39:26 +0530

    Modified App.jsx and adding From.jsx by deleting Input.jsx and Login.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,12 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
-let isLoggedIn = false;
-let currentTime = new Date().getHours();
-// console.log(currentTime);
+let userIsRegistered = true;
 
 function App() {
   return (
     <div className="container">
-      {
-        isLoggedIn ?
-        <div>
-          <h1>Hello User</h1>
-          {currentTime >= 20 && <h1>Why are you still working? Have some rest.</h1>}
-        </div>
-        :
-        <div>
-          <h1>Hello</h1>
-          <Login />
-          {currentTime >= 20 && <h1>Why are you still working? Have some rest.</h1>}
-        </div>
-      }
+      <Form isRegistered={userIsRegistered}/>
     </div>
   );
 }
